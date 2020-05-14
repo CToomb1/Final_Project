f1bcf096127712ace8cda22544d7da7f2b3db3f4
Alex Rosenkranz <arosenkranz@2u.com>
2020-05-14 18:18:02 -0400

    deploy

--- client/src/app.js
+++ client/src/app.js
@@ -1,53 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import SearchRecipes from "./pages/searchrecipespage";
-import SavedRecipes from "./pages/savedrecipespage";
-import Navbar from "./components/navbarcomponent";
+import SearchRecipes from './pages/searchrecipespage';
+import SavedRecipes from './pages/savedrecipespage';
+import Navbar from './components/navbarcomponent';
 
-import * as API from "./utils/apiutils";
-import AuthService from "./utils/authutils";
+import * as API from './utils/apiutils';
+import AuthService from './utils/authutils';
 
-import UserInfoContext from "./utils/userinfocontextutils";
+import UserInfoContext from './utils/userinfocontextutils';
 
 function App() {
+  const [userInfo, setUserInfo] = useState({
+    savedRecipes: [],
+    username: '',
+    getUserData: () => {
+      const token = AuthService.loggedIn() ? AuthService.getToken() : null;
+      if (!token) {
+        return false;
+      }
+      API.getMe(token)
+        .then(({ data: { username, savedRecipes } }) => setUserInfo({ ...userInfo, username, savedRecipes }))
+        .catch((err) => console.log(err));
+    },
+  });
 
-    const [userInfo, setUserInfo] = useState({
+  useEffect(() => {
+    userInfo.getUserData();
+  }, []);
 
-        savedRecipes: [],
-        username: "",
-        getUserData: () => {
-
-            const token = AuthService.loggedIn() ? AuthService.getToken() : null;
-            if (!token) {
-                return false;
-            }
-            API.getMe(token)
-                .then(({ data: { username, savedRecipes } }) =>
-                    setUserInfo({ ...userInfo, username, savedRecipes })
-                )
-                .catch((err) => console.log(err));
-        },
-    });
-
-
-    useEffect(() => {
-        userInfo.getUserData();
-    });
-
-    return (
-        <Router>
-            <>
-                <UserInfoContext.Provider value={userInfo}>
-                    <Navbar />
-                    <Switch>
-                        <Route exact path='/' component={SearchRecipes} />
-                        <Route exact path='/saved' component={SavedRecipes} />
-                        <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-                    </Switch>
-                </UserInfoContext.Provider>
-            </>
-        </Router>
-    );
+  return (
+    <Router>
+      <>
+        <UserInfoContext.Provider value={userInfo}>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={SearchRecipes} />
+            <Route exact path='/saved' component={SavedRecipes} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Switch>
+        </UserInfoContext.Provider>
+      </>
+    </Router>
+  );
 }
 
 export default App;
